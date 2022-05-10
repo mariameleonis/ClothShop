a6b6abdfc281173475a6e7bd51aa0220c10f5ba1
Mariya <russakova.m@gmail.com>
2022-05-10 23:45:49 +0600

    test fails

--- src/main/java/com/epam/clothshop/service/CategoryService.java
+++ src/main/java/com/epam/clothshop/service/CategoryService.java
@@ -1,11 +1,18 @@
 package com.epam.clothshop.service;
 
 import com.epam.clothshop.dto.CategoryDto;
+import com.epam.clothshop.model.Category;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
     public Long createCategory(CategoryDto categoryDto) {
+        return null;
+    }
+
+    public List<Category> getCategories() {
         return null;
     }
 }
--- src/test/java/com/epam/clothshop/rest/CategoryControllerTest.java
+++ src/test/java/com/epam/clothshop/rest/CategoryControllerTest.java
@@ -1,6 +1,7 @@
 package com.epam.clothshop.rest;
 
 import com.epam.clothshop.dto.CategoryDto;
+import com.epam.clothshop.model.Category;
 import com.epam.clothshop.service.CategoryService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -14,13 +15,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -65,5 +70,32 @@ public class CategoryControllerTest {
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(categoryDto)))
                 .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testGetAllCategories_WhenEverythingIsOk() throws Exception {
+
+        when(categoryService.getCategories()).thenReturn(List.of(createCategory(1L, "Dresses"),
+                createCategory(2L, "Skirts"), createCategory(3L, "Tops")));
+
+        mockMvc.perform(get("/api/categories"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].categoryName", is("Dresses")))
+                .andExpect(jsonPath("$[0].categoryId", is(1L)))
+                .andExpect(jsonPath("$[1].categoryName", is("Skirts")))
+                .andExpect(jsonPath("$[1].categoryId", is(2L)))
+                .andExpect(jsonPath("$[2].categoryName", is("Tops")))
+                .andExpect(jsonPath("$[2].categoryId", is(3L)));
+    }
+
+    private Category createCategory(long id, String name) {
+
+        Category category = new Category();
+        category.setCategoryId(id);
+        category.setCategoryName(name);
+
+        return  category;
     }
 }

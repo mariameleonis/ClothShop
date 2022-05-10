7d1bb260a05edc6f3f6911208ecdb12e22b1cc69
Mariya <russakova.m@gmail.com>
2022-05-10 22:40:25 +0600

    CategoryControllerTest fails

--- src/test/java/com/epam/clothshop/rest/CategoryControllerTest.java
+++ src/test/java/com/epam/clothshop/rest/CategoryControllerTest.java
@@ -1,16 +1,55 @@
 package com.epam.clothshop.rest;
 
+import com.epam.clothshop.dto.CategoryDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CategoryController.class)
 @SpringBootTest
 public class CategoryControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private CategoryService categoryService;
+
+    @Captor
+    private ArgumentCaptor<CategoryDto> argumentCaptor;
+
+    @Test
+    public void testCreateCategory_WhenEverythingIsOk() throws Exception {
+
+        CategoryDto categoryDto = new CategoryDto();
+        categoryDto.setCategoryName("Dresses");
+
+        when(categoryService.createCategory(argumentCaptor.capture())).thenReturn(1L);
+
+        mockMvc.perform(post("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(categoryDto)))
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("Location"))
+                .andExpect(header().string("Location", "http://localhost/api/categories/1"));
+
+        assertThat(argumentCaptor.getValue().getCategoryName(), is("Dresses"));
+    }
 }

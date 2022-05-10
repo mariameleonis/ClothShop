b22d0c73cf40d0ccce8d2b6d1e7553b23a123ff5
Mariya <russakova.m@gmail.com>
2022-05-10 20:42:35 +0600

    add CategoryControllerTest (fails)

---
+++ src/test/java/com/epam/clothshop/rest/CategoryControllerTest.java
@@ -0,0 +1,16 @@
+package com.epam.clothshop.rest;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(CategoryController.class)
+@SpringBootTest
+public class CategoryControllerTest {
+
+}

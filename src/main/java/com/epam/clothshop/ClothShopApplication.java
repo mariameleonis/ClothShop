3698d457e7e693428208c1ab682be3f30afa7bb3
Mariya <russakova.m@gmail.com>
2022-05-19 19:37:10 +0600

    add modelMapper configuration

--- src/main/java/com/epam/clothshop/ClothShopApplication.java
+++ src/main/java/com/epam/clothshop/ClothShopApplication.java
@@ -1,12 +1,15 @@
 package com.epam.clothshop;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.context.annotation.Bean;
+
+import static org.modelmapper.config.Configuration.AccessLevel.PRIVATE;
 
 @SpringBootApplication
 public class ClothShopApplication {
@@ -18,6 +21,12 @@ public class ClothShopApplication {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true)
+                .setFieldAccessLevel(PRIVATE);
+        return mapper;
     }
 }

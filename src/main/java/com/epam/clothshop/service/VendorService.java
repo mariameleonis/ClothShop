609cd700832687bf1639cb5475c1364491fb4e5b
Mariya <russakova.m@gmail.com>
2022-05-19 19:35:42 +0600

    add createVendor to VendorService

--- src/main/java/com/epam/clothshop/service/VendorService.java
+++ src/main/java/com/epam/clothshop/service/VendorService.java
@@ -1,24 +1,38 @@
 package com.epam.clothshop.service;
 
+import com.epam.clothshop.dao.VendorRepository;
 import com.epam.clothshop.dto.CategoryDto;
 import com.epam.clothshop.dto.ProductDto;
 import com.epam.clothshop.dto.VendorDto;
 import com.epam.clothshop.model.Vendor;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 @Service
 public class VendorService {
+
+    @Autowired
+    private VendorRepository vendorRepository;
+
     public Long createVendor(VendorDto vendorDto) {
-        return null;
+
+        Vendor vendor = new Vendor();
+
+        vendor.setVendorName(vendorDto.getVendorName());
+
+        vendor = vendorRepository.save(vendor);
+
+        return vendor.getVendorId();
     }
 
     public List<Vendor> getVendors() {
         return null;
     }
 
-    public Optional<Vendor> getVendorById(long id) {
+    public Vendor getVendorById(long id) {
         return null;
     }
 
@@ -30,6 +44,7 @@ public class VendorService {
         return null;
     }
 
-    public void deleteVendorById(Long id) {
+    public Vendor deleteVendorById(Long id) {
+        return null;
     }
 }

b0709f2f7de3761f264a0501208ccdad00e76de5
Mariya <russakova.m@gmail.com>
2022-05-27 02:19:51 +0600

    UserControllerTest succeeds

---
+++ src/main/java/com/epam/clothshop/rest/UserController.java
@@ -0,0 +1,56 @@
+package com.epam.clothshop.rest;
+
+import com.epam.clothshop.dto.UserDto;
+import com.epam.clothshop.dto.VendorDto;
+import com.epam.clothshop.model.User;
+import com.epam.clothshop.model.Vendor;
+import com.epam.clothshop.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    UserServiceImpl userService;
+
+    @PostMapping
+    public ResponseEntity<Void> createUser(@Validated(UserDto.New.class) @RequestBody UserDto userDto,
+                                           UriComponentsBuilder uriComponentsBuilder) {
+        Long id = userService.createUser(userDto);
+
+        UriComponents uriComponents = uriComponentsBuilder.path("/api/users/{id}").buildAndExpand(id);
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(uriComponents.toUri());
+
+        return new ResponseEntity<>(header, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok(userService.getUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id") Long id, @Validated(UserDto.Update.class) @RequestBody UserDto userDto) {
+        return ResponseEntity.ok(userService.updateUser(userDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUserById(@PathVariable("id") Long id) {
+       userService.deleteUserById(id);
+    }
+}

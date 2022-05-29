4eb27e1edb2adcc362ec1bf5b647544c6b6173bf
Mariya <russakova.m@gmail.com>
2022-05-30 01:19:55 +0600

    add more test data

--- src/test/java/com/epam/clothshop/ClothShopTestData.java
+++ src/test/java/com/epam/clothshop/ClothShopTestData.java
@@ -2,13 +2,13 @@ package com.epam.clothshop;
 
 import com.epam.clothshop.dto.ProductDto;
 import com.epam.clothshop.dto.UserDto;
-import com.epam.clothshop.model.Product;
-import com.epam.clothshop.model.User;
-import com.epam.clothshop.model.Vendor;
+import com.epam.clothshop.model.*;
+import org.aspectj.weaver.ast.Or;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class ClothShopTestData {
     public static final Product PRODUCT_1 = new Product(1L, "Little Black Dress", BigDecimal.valueOf(120.50), 15, 1L, 1L);
@@ -35,10 +35,14 @@ public class ClothShopTestData {
     public static final User USER_3 = new User(3L, "svetlana999", "Svetlana", "Petrova", "petrova.s@gmail.com", "12345", "77056056789", null);
 
     public static final List<User> USERS_LIST = new ArrayList<>(List.of(USER_1, USER_2, USER_3));
-
     public static final UserDto VALID_USER_DTO = new UserDto("svetlana999", "Svetlana", "Petrova", "petrova.s@gmail.com", "12345", "77056056789");
-
     public static final UserDto INVALID_USER_DTO_UPDATE = new UserDto(42L, "svetlana999", "Svetlana", "Petrova", "petrova.s@gmail.com", "12345", "77056056789");
     public static final UserDto INVALID_USER_DTO = new UserDto("", "", "", "", "", "");
-
+    public static final OrderItem ORDER_ITEM_1 = new OrderItem(1L,1,BigDecimal.valueOf(120.50));
+    public static final OrderItem ORDER_ITEM_2 = new OrderItem(2L,2,BigDecimal.valueOf(110.70));
+    public static final OrderItem ORDER_ITEM_3 = new OrderItem(3L,1,BigDecimal.valueOf(100.90));
+    public static final Order ORDER_1 = new Order(USER_1, BigDecimal.valueOf(120.50), Set.of(ORDER_ITEM_1));
+    public static final Order ORDER_2 = new Order(USER_2, BigDecimal.valueOf(321.40), Set.of(ORDER_ITEM_1, ORDER_ITEM_3));
+    public static final Order ORDER_3 = new Order(USER_3, BigDecimal.valueOf(100.90), Set.of(ORDER_ITEM_3));
+    public static final List<Order> ORDER_LIST = new ArrayList<>(List.of(ORDER_1, ORDER_2, ORDER_3));
 }

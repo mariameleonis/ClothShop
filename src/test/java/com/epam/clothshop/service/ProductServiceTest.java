516d014a118b000cbffbef1cc1ab01299b51e065
Mariya <russakova.m@gmail.com>
2022-05-29 19:00:58 +0600

    ProductServiceTest fails

--- src/test/java/com/epam/clothshop/service/ProductServiceTest.java
+++ src/test/java/com/epam/clothshop/service/ProductServiceTest.java
@@ -1,2 +1,100 @@
-package com.epam.clothshop.service;public class ProductServiceTest {
+package com.epam.clothshop.service;
+
+import com.epam.clothshop.dao.ProductRepository;
+import com.epam.clothshop.dto.ProductDto;
+import com.epam.clothshop.model.Product;
+import com.epam.clothshop.model.User;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.epam.clothshop.ClothShopTestData.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ProductServiceTest {
+    @Mock
+    private ProductRepository productRepository;
+
+    @Mock
+    private ModelMapper productMapper;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @Captor
+    private ArgumentCaptor<Product> argumentCaptor;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Test
+    public void testGetAllProducts() {
+
+        when(productRepository.findAll()).thenReturn(PRODUCT_LIST);
+
+        List<Product> resultProducts = productService.getProducts();
+
+        assertEquals(resultProducts, PRODUCT_LIST);
+    }
+
+    @Test
+    public void testGetProductById() {
+
+        when(productRepository.findById(PRODUCT_1.getId())).thenReturn(Optional.of(PRODUCT_1));
+
+        Product resultProduct = productService.getProductById(PRODUCT_1.getId());
+
+        assertEquals(resultProduct, PRODUCT_1);
+    }
+
+    @Test
+    public void testCreateProduct() {
+
+        when(productMapper.map(VALID_PRODUCT_DTO, User.class)).thenReturn(PRODUCT_3);
+
+        when(productRepository.save(argumentCaptor.capture())).thenReturn(PRODUCT_3);
+
+        Long resultProductId = productService.createProduct(VALID_PRODUCT_DTO);
+
+        assertThat(resultProductId, is(PRODUCT_3.getId()));
+        assertThat(argumentCaptor.getValue().getName(), is(PRODUCT_3.getName()));
+    }
+
+    @Test
+    public void testUpdateProduct() {
+
+        ProductDto productDto = modelMapper.map(PRODUCT_1_UPDATE, ProductDto.class);
+
+        when(productRepository.findById(productDto.getId())).thenReturn(Optional.of(PRODUCT_1));
+
+        when(productMapper.map(productDto, Product.class)).thenReturn(PRODUCT_1_UPDATE);
+
+        when(productRepository.save(PRODUCT_1_UPDATE)).thenReturn(PRODUCT_1_UPDATE);
+
+        Product product = productService.updateProduct();
+
+        assertEquals(product, PRODUCT_1_UPDATE);
+    }
+
+    @Test
+    public void testDeleteProductById() {
+
+        when(productRepository.findById(PRODUCT_3.getId())).thenReturn(Optional.of(PRODUCT_3));
+
+        doNothing().when(productRepository).deleteById(PRODUCT_3.getId());
+
+        productService.deleteProductById(PRODUCT_3.getId());
+
+        verify(productRepository, times(1)).deleteById(PRODUCT_3.getId());
+    }
 }

9163552ee500908bcbc3bc4e59350a24e56a12f5
Mariya <russakova.m@gmail.com>
2022-06-06 13:32:05 +0600

    add Role.java

--- src/main/java/com/epam/clothshop/model/Role.java
+++ src/main/java/com/epam/clothshop/model/Role.java
@@ -1,2 +1,27 @@
-package com.epam.clothshop.model;public class Role {
+package com.epam.clothshop.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+
+    public static final String CUSTOMER = "CUSTOMER";
+    public static final String EDITOR = "EDITOR";
+    public static final String ADMIN = "ADMIN";
+
+    private String authority;
+
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
